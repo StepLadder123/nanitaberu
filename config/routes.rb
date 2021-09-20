aff166fc92ee0f50cae42bd7c5a522b1f54ac290
StepLadder123 <makito.takatori@gmail.com>
2021-09-20 18:13:44 +0900

    ルーティングの編集
    - root toの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'users#index'
+  get 'rooms/index'
+  root to: 'rooms#index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: :index
