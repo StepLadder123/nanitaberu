7f67a8dc31d37a75f0696b280836a53051497e5d
StepLadder123 <makito.takatori@gmail.com>
2021-09-21 12:39:58 +0900

    マイグレーションの編集

--- db/schema.rb
+++ db/schema.rb
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2021_09_21_013143) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "name", default: "", null: false
+    t.string "room_key", default: ""
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
