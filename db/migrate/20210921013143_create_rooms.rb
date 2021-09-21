459725d59fc68eb1531394d5d168da7e536d2a16
StepLadder123 <makito.takatori@gmail.com>
2021-09-21 11:16:57 +0900

    マイグレーションファイルの編集
    - roomsテーブルの記述

--- db/migrate/20210921013143_create_rooms.rb
+++ db/migrate/20210921013143_create_rooms.rb
@@ -1,7 +1,9 @@
 class CreateRooms < ActiveRecord::Migration[6.0]
   def change
     create_table :rooms do |t|
-
+      t.string :room_name, null: false
+      t.string :key,       null: false, uniqueness: true
+      t.text   :rule
       t.timestamps
     end
   end
