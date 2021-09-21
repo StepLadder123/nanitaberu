248da53bac3fe46f40589fe2a37cd26225e1f5be
StepLadder123 <makito.takatori@gmail.com>
2021-09-21 11:20:44 +0900

    ルーティングの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'rooms/index'
   root to: 'rooms#index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: :index
+  resources :rooms, only: [:index, :new, :create]
 end
