2e55d713664d011ca8b3d7772296335059577e8f
StepLadder123 <makito.takatori@gmail.com>
2021-09-24 18:28:52 +0900

    ルーティングの設定
    - roomsコントローラーのshow, approve, rejectアクションの生成

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
   root to: 'rooms#index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :rooms, only: [:index, :new, :create] do
+  resources :rooms, only: [:index, :new, :create, :show] do
     collection do
       get 'search'
       get 'join'
       get 'wait'
       get 'cancel'
+      get 'approve/:id', to: 'rooms#approve', as: :approve
+      get 'reject/:id', to: 'rooms#reject', as: :reject
     end
   end
 end
