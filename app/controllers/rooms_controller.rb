84f49975d4b90fcb5947dab197db57cde73d8d16
StepLadder123 <makito.takatori@gmail.com>
2021-09-24 18:32:00 +0900

    roomsコントローラーの編集
    - indexアクションでログインしたルーム登録済みユーザーのshowアクションへの遷移を記述
    - show, approve, rejectアクションを記述

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -7,10 +7,13 @@ class RoomsController < ApplicationController
         redirect_to wait_rooms_path
       elsif current_user.room_key == ""
         redirect_to search_rooms_path
+      else
+        @room = Room.find(current_user.room_key)
+        redirect_to room_path(@room.id)
       end
     end
   end
-
+  
   def new
     @room = Room.new
   end
@@ -27,6 +30,11 @@ class RoomsController < ApplicationController
     end
   end
   
+  def show
+    @room = Room.find(current_user.room_key)
+    @applies = User.where(waiting_room_key: @room.id)
+  end
+
   def search
     @rooms = Room.includes(:room)
   end
@@ -53,14 +61,53 @@ class RoomsController < ApplicationController
     current_user.save
     redirect_to root_path
   end
-
+  
+  def approve
+    delete_waiting
+    @room = Room.find(current_user.room_key)
+    @user.room_key = @room.id
+    if @room.member1 == nil
+      @room.member1 = @user.id
+    elsif @room.member2 == nil
+      @room.member2 = @user.id
+    elsif @room.member3 == nil
+      @room.member3 = @user.id
+    elsif @room.member4 == nil
+      @room.member4 = @user.id
+    elsif @room.member5 == nil
+      @room.member5 = @user.id
+    elsif @room.member6 == nil
+      @room.member6 = @user.id
+    elsif @room.member7 == nil
+      @room.member7 = @user.id
+    elsif @room.member8 == nil
+      @room.member8 = @user.id
+    elsif @room.member9 == nil
+      @room.member9 = @user.id
+    end
+    @user.save
+    @room.save
+    redirect_to root_path
+  end
+  
+  def reject
+    delete_waiting
+    @user.save
+    redirect_to root_path
+  end
+  
   private
-
+  
   def room_params
     params.require(:room).permit(:room_name, :key, :rule).merge(user_id: current_user.id)
   end
-
+  
   def search_room
     @r = Room.ransack(params[:q])
+  end
+  
+  def delete_waiting
+    @user = User.find(params[:id])
+    @user.waiting_room_key = ""
   end
 end
