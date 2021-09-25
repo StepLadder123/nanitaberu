cd70bb0dbbe8969598f94a3237bf71af08666a74
StepLadder123 <makito.takatori@gmail.com>
2021-09-25 17:46:41 +0900

    マイグレーションファイルの生成
    - menuテーブル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_24_005603) do
+ActiveRecord::Schema.define(version: 2021_09_24_114655) do
+
+  create_table "menus", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "cook_name", null: false
+    t.text "memo"
+    t.bigint "room_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_menus_on_room_id"
+  end
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "room_name", null: false
@@ -46,5 +55,6 @@ ActiveRecord::Schema.define(version: 2021_09_24_005603) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "menus", "rooms"
   add_foreign_key "rooms", "users"
 end
