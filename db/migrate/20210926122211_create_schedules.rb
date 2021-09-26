77fcce285e44659038cbf511a25a1df6ecee9018
StepLadder123 <makito.takatori@gmail.com>
2021-09-26 23:22:02 +0900

    マイグレーションファイルの編集

--- db/migrate/20210926122211_create_schedules.rb
+++ db/migrate/20210926122211_create_schedules.rb
@@ -1,7 +1,8 @@
 class CreateSchedules < ActiveRecord::Migration[6.0]
   def change
     create_table :schedules do |t|
-
+      t.date    :date,    presence: true
+      t.integer :menu_id, presence: true
       t.timestamps
     end
   end
